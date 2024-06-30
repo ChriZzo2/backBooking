a3ba1e3e778034617fa182601442f7582de5c28e
Chrizzoman <sasuke123451997@gmail.com>
2024-06-30 16:30:26 +0300

    refactoring

--- config/middlewares.js
+++ config/middlewares.js
@@ -2,13 +2,7 @@ module.exports = [
   'strapi::logger',
   'strapi::errors',
   'strapi::security',
-  {
-    name: 'strapi::cors',
-    config: {
-      enable: true,
-      origin: ['https://booking-ruby-nine.vercel.app/', 'https://booking-5tvt7h05z-chrizzo2s-projects.vercel.app/', 'https://wealthy-broccoli-070ad62d8a.strapiapp.com/']
-    }
-  },
+  'strapi::cors',
   'strapi::poweredBy',
   'strapi::query',
   'strapi::body',
